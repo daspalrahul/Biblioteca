6668d76e06f15cef41bddb7d9141830f607f9b70
Rahul Daspal <daspalrahul@gmail.com>
2015-07-28 11:28:26 +0530

    #0005|Verified if the message displayed is actually correct

--- src/com/twu/biblioteca/View.java
+++ src/com/twu/biblioteca/View.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+
 public class View {
     public void displayWelcomeMessage() {
         System.out.println("Welcome");
--- test/com/twu/biblioteca/ViewTest.java
+++ test/com/twu/biblioteca/ViewTest.java
@@ -1,15 +1,26 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ViewTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Test
     public void viewKnowsHowToDisplayWelcomeMessage() {
@@ -20,4 +31,17 @@ public class ViewTest {
         Mockito.verify(view).displayWelcomeMessage();
     }
 
+    @Test
+    public void viewsKnowsThatTheWelcomeMessageIsCorrect() {
+        View view = new View();
+
+        view.displayWelcomeMessage();
+
+        assertEquals("Welcome\n", outContent.toString());
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(System.out);
+    }
 }
