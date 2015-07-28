79f60a47f688e834df77dfc37961103617bcb4d3
Rahul Daspal <daspalrahul@gmail.com>
2015-07-28 11:14:37 +0530

    #0004|Added a view to display the welcome message

---
+++ src/com/twu/biblioteca/View.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+public class View {
+    public void displayWelcomeMessage() {
+        System.out.println("Welcome");
+    }
+}
---
+++ test/com/twu/biblioteca/ViewTest.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ViewTest {
+
+    @Test
+    public void viewKnowsHowToDisplayWelcomeMessage() {
+        View view = Mockito.mock(View.class);
+
+        view.displayWelcomeMessage();
+
+        Mockito.verify(view).displayWelcomeMessage();
+    }
+
+}
