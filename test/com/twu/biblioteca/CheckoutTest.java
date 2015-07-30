b0df25405d075d288ca26bc23add6a5e13d711f2
Rahul Daspal <daspalrahul@gmail.com>
2015-07-30 09:52:16 +0530

    #0037|Verified that checking out an unavailable book returns an unsuccessful checkout message

--- test/com/twu/biblioteca/CheckoutTest.java
+++ test/com/twu/biblioteca/CheckoutTest.java
@@ -2,11 +2,8 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 import org.mockito.Mockito;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import static org.junit.Assert.*;
 
 public class CheckoutTest {
 
@@ -22,5 +19,19 @@ public class CheckoutTest {
         checkout.execute();
 
         Mockito.verify(view).displaySuccessfulCheckOutMessage();
+    }
+
+    @Test
+    public void checkoutCanNotCheckoutAGameOfThronesFromLibraryThatDoesNotContainsTheSame() {
+        String[] book = {"A Game Of Thrones", "George R.R. Martin", "1996", "available"};
+        ArrayList<String[]> list = new ArrayList<>(Arrays.<String[]>asList(book));
+        Library library = new Library(list);
+        View view = Mockito.mock(View.class);
+        Checkout checkout = new Checkout(view, library);
+
+        Mockito.when(view.getUserInput()).thenReturn("Cupcakes and Tiaras");
+        checkout.execute();
+
+        Mockito.verify(view).displayUnSuccessfulCheckOutMessage();
     }
 }
