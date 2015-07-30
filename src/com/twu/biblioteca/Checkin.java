a3dc5b5bf913a300639e00ff88483a0542b59c71
Rahul Daspal <daspalrahul@gmail.com>
2015-07-30 10:25:07 +0530

    #0040|Added a library operation so that biblioteca app can perform checkin

--- src/com/twu/biblioteca/Checkin.java
+++ src/com/twu/biblioteca/Checkin.java
@@ -1,4 +1,17 @@
 package com.twu.biblioteca;
 
-public class Checkin {
+public class Checkin implements LibraryOperation {
+
+    View view;
+    Library library;
+
+    public Checkin(View view, Library library) {
+        this.view = view;
+        this.library = library;
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
--- test/com/twu/biblioteca/CheckinTest.java
+++ test/com/twu/biblioteca/CheckinTest.java
@@ -1,7 +1,18 @@
 package com.twu.biblioteca;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.mockito.Mockito;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CheckinTest {
 
+    @Test
+    public void checkinIsUnsuccessfulWhenBookDoesNotBelongToLibary() {
+        String[] book = {"A Game Of Thrones", "George R.R. Martin", "1996", "available"};
+        ArrayList<String[]> list = new ArrayList<>(Arrays.<String[]>asList(book));
+        Library library = new Library(list);
+        View view = Mockito.mock(View.class);
+        Checkout checkout = new Checkout(view, library);
+    }
 }
