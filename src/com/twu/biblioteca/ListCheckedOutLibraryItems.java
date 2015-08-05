9333562853150fcfce7db92e3eb0906af709886c
Rahul Daspal <daspalrahul@gmail.com>
2015-08-05 10:21:33 +0530

    #0082|Added an operation to list all the checked out books and their respective borrowers

--- src/com/twu/biblioteca/ListCheckedOutLibraryItems.java
+++ src/com/twu/biblioteca/ListCheckedOutLibraryItems.java
@@ -1,4 +1,18 @@
 package com.twu.biblioteca;
 
-public class ListCheckedOutLibraryItems {
+public class ListCheckedOutLibraryItems implements LibraryOperation {
+
+
+    private View view;
+    private Section section;
+
+    public ListCheckedOutLibraryItems(View view, Section section) {
+        this.view = view;
+        this.section = section;
+    }
+
+    @Override
+    public void execute() {
+        view.displayListOfAllCheckedOutLibraryItems(section.listOfAllCheckedOutLIbraryItems());
+    }
 }
---
+++ test/com/twu/biblioteca/ListCheckedOutLibraryItemsTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+
+public class ListCheckedOutLibraryItemsTest {
+
+    @Test
+    public void listCheckedOutLibraryItemsKnowsHowToGetListOfCheckedOutLibraryItems() {
+        View view = Mockito.mock(View.class);
+        Section section = Mockito.mock(Section.class);
+        ListCheckedOutLibraryItems listCheckedOutLibraryItems = new ListCheckedOutLibraryItems(view, section);
+
+        listCheckedOutLibraryItems.execute();
+
+        Mockito.verify(view).displayListOfAllCheckedOutLibraryItems(anyString());
+    }
+}
