eb50127df6868ebc85db2eafab0f3e86161812f3
Rahul Daspal <daspalrahul@gmail.com>
2015-08-09 12:28:56 +0530

    #0111|Added checkout movie view that can draw and return the next view

--- src/com/twu/biblioteca/view/CheckoutMovieView.java
+++ src/com/twu/biblioteca/view/CheckoutMovieView.java
@@ -1,13 +1,35 @@
 package com.twu.biblioteca.view;
 
-public class CheckoutMovieView implements IView{
+
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Section;
+import com.twu.biblioteca.model.Session;
+
+
+public class CheckoutMovieView implements IView {
+
+    private Console console;
+    private IView userMenuView;
+    private Session session;
+    private Section movies;
+
+    public CheckoutMovieView(Console console, IView userMenuView, Session session, Section movies) {
+        this.console = console;
+        this.userMenuView = userMenuView;
+        this.session = session;
+        this.movies = movies;
+    }
+
     @Override
     public void draw() {
-
+        if (movies.checkOut(console.getInput(), session.getCurrentUser()))
+            console.printOutput("Thank you! Enjoy the movie");
+        else
+            console.printOutput("That movie is not available.");
     }
 
     @Override
     public IView next() {
-        return null;
+        return userMenuView;
     }
 }
---
+++ test/com/twu/biblioteca/view/CheckoutMovieViewTest.java
@@ -0,0 +1,45 @@
+package com.twu.biblioteca.view;
+
+
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Section;
+import com.twu.biblioteca.model.Session;
+import com.twu.biblioteca.model.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+
+
+public class CheckoutMovieViewTest {
+
+    @Test
+    public void checkoutMovieViewKnowsHowToDrawItself() {
+        Console console = Mockito.mock(Console.class);
+        Section movies = Mockito.mock(Section.class);
+        Session session = Mockito.mock(Session.class);
+        User user = Mockito.mock(User.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        IView checkoutBookView = new CheckoutBookView(console, userMenuView, session, movies);
+
+        Mockito.when(session.getCurrentUser()).thenReturn(user);
+        Mockito.when(console.getInput()).thenReturn("Kick");
+        checkoutBookView.draw();
+
+        Mockito.verify(movies).checkOut(anyString(), eq(user));
+    }
+
+    @Test
+    public void checkoutMovieViewKnowsHowToReturnTheNextView() {
+        Console console = Mockito.mock(Console.class);
+        Section movies = Mockito.mock(Section.class);
+        Session session = Mockito.mock(Session.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        IView checkoutBookView = new CheckoutBookView(console, userMenuView, session, movies);
+
+        assertEquals(userMenuView, checkoutBookView.next());
+    }
+
+}
