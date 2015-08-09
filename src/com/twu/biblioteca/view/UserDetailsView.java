3d02204ea98cc268edb2ad905d562e4c34d50b51
Rahul Daspal <daspalrahul@gmail.com>
2015-08-09 16:55:50 +0530

    #0115|Added a user details view that can give user deatils as well as the next view

--- src/com/twu/biblioteca/view/UserDetailsView.java
+++ src/com/twu/biblioteca/view/UserDetailsView.java
@@ -1,13 +1,27 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Session;
+
 public class UserDetailsView implements IView {
+
+    private Console console;
+    private IView userMenuView;
+    private Session session;
+
+    public UserDetailsView(Console console, IView userMenuView, Session session) {
+        this.console = console;
+        this.userMenuView = userMenuView;
+        this.session = session;
+    }
+
     @Override
     public void draw() {
-
+        console.printOutput(session.getCurrentUser());
     }
 
     @Override
     public IView next() {
-        return null;
+        return userMenuView;
     }
 }
---
+++ test/com/twu/biblioteca/view/UserDetailsViewTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca.view;
+
+
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Session;
+import com.twu.biblioteca.model.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+
+public class UserDetailsViewTest {
+
+    @Test
+    public void userDetailsViewKnowsHowToDrawItself() {
+        Console console = Mockito.mock(Console.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        Session session = Mockito.mock(Session.class);
+        User user = Mockito.mock(User.class);
+        IView userDetailsView = new UserDetailsView(console, userMenuView, session);
+
+        Mockito.when(session.getCurrentUser()).thenReturn(user);
+        userDetailsView.draw();
+
+        Mockito.verify(console).printOutput(user);
+    }
+
+    @Test
+    public void userDetailsViewKnowsWhatViewToGoToNext() {
+        Console console = Mockito.mock(Console.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        Session session = Mockito.mock(Session.class);
+        IView userDetailsView = new UserDetailsView(console, userMenuView, session);
+
+        assertEquals(userMenuView, userDetailsView.next());
+    }
+}
