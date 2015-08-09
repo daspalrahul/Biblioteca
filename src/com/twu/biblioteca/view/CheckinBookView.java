3823f11ca50b8a35eddcf9c08a43c60b9d67f465
Rahul Daspal <daspalrahul@gmail.com>
2015-08-09 17:05:46 +0530

    #0116|Added checkin book view that can checkin books to the library

--- src/com/twu/biblioteca/view/CheckinBookView.java
+++ src/com/twu/biblioteca/view/CheckinBookView.java
@@ -1,13 +1,33 @@
 package com.twu.biblioteca.view;
 
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Section;
+import com.twu.biblioteca.model.Session;
+
 public class CheckinBookView implements IView {
+
+    private Console console;
+    private IView userMenuView;
+    private Session session;
+    private Section books;
+
+    public CheckinBookView(Console console, IView userMenuView, Session session, Section books) {
+        this.console = console;
+        this.userMenuView = userMenuView;
+        this.session = session;
+        this.books = books;
+    }
+
     @Override
     public void draw() {
-
+        if (books.checkIn(console.getInput(), session.getCurrentUser()))
+            console.printOutput("Thank you for returning the book.");
+        else
+            console.printOutput("That is not a valid book to return.");
     }
 
     @Override
     public IView next() {
-        return null;
+        return userMenuView;
     }
 }
---
+++ test/com/twu/biblioteca/view/CheckinBookViewTest.java
@@ -0,0 +1,44 @@
+package com.twu.biblioteca.view;
+
+
+import com.twu.biblioteca.Console;
+import com.twu.biblioteca.model.Section;
+import com.twu.biblioteca.model.Session;
+import com.twu.biblioteca.model.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+
+
+public class CheckinBookViewTest {
+
+    @Test
+    public void checkInBookViewKnowsHowToDrawItself() {
+        Console console = Mockito.mock(Console.class);
+        Section books = Mockito.mock(Section.class);
+        Session session = Mockito.mock(Session.class);
+        User user = Mockito.mock(User.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        IView checkinBookView = new CheckinBookView(console, userMenuView, session, books);
+
+        Mockito.when(session.getCurrentUser()).thenReturn(user);
+        Mockito.when(console.getInput()).thenReturn("A Game of Thrones");
+        checkinBookView.draw();
+
+        Mockito.verify(books).checkIn(anyString(), eq(user));
+    }
+
+    @Test
+    public void checkinBookViewKnowsHowToReturnTheNextView() {
+        Console console = Mockito.mock(Console.class);
+        Section books = Mockito.mock(Section.class);
+        Session session = Mockito.mock(Session.class);
+        IView userMenuView = Mockito.mock(UserMenuView.class);
+        IView checkinBookView = new CheckinBookView(console, userMenuView, session, books);
+
+        assertEquals(userMenuView, checkinBookView.next());
+    }
+}
